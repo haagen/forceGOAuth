42c35c2024fdbffb410fd274f12f321e2acf9340
Martin Haagen <martin@mbh.se>
2014-06-16 22:45:02 +0200

    Created more test methods - coverage at ~69% Ran `go fmt`

--- forceGOAuth.go
+++ forceGOAuth.go
@@ -2,9 +2,9 @@ package forceGOAuth
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
-	"encoding/json"
 	"html/template"
 	"io"
 	"io/ioutil"
@@ -18,7 +18,7 @@ const (
 )
 
 const (
-	GrantType_AuthorizationToken = iota 
+	GrantType_AuthorizationToken = iota
 	GrantType_RefreshToken
 	GrantType_Password
 )
@@ -32,27 +32,27 @@ const (
 var HTTPClient *http.Client = nil
 
 type OAuthSecurity struct {
-    ConsumerKey string
-    ConsumerSecret string
-    ConsumerCallback string
-    OAuthBaseURL string
-    AuthCode string
-    AccessToken string
-    RefreshToken string
-    InstanceUrl string
-    Id string
-    IssuedAt string
-    Signature string
-    Scope string
-    AutoRefreshToken bool
-    UserName string
-    Password string
+	ConsumerKey      string
+	ConsumerSecret   string
+	ConsumerCallback string
+	OAuthBaseURL     string
+	AuthCode         string
+	AccessToken      string
+	RefreshToken     string
+	InstanceUrl      string
+	Id               string
+	IssuedAt         string
+	Signature        string
+	Scope            string
+	AutoRefreshToken bool
+	UserName         string
+	Password         string
 }
 
 type ForceResponse map[string]interface{}
 
-func NewOAuthSecurity()(oa *OAuthSecurity) {
-	oa = &OAuthSecurity {
+func NewOAuthSecurity() (oa *OAuthSecurity) {
+	oa = &OAuthSecurity{
 		AutoRefreshToken: true,
 	}
 	return
@@ -68,8 +68,8 @@ func (oa *OAuthSecurity) ExportSettingsJSON() (settingsJson []byte, err error) {
 	return
 }
 
-func (oa *OAuthSecurity) DoFullWebServerFlow()(err error) {
-	oa.ConsumerCallback = "http://localhost:"+PORT
+func (oa *OAuthSecurity) DoFullWebServerFlow() (err error) {
+	oa.ConsumerCallback = "http://localhost:" + PORT
 	ch := make(chan OAuthSecurity)
 	_, err = startLocalHttpServer(ch)
 	Open(oa.BuildAuthorizeURL(OAuthFlow_WebServer))
@@ -83,15 +83,15 @@ func (oa *OAuthSecurity) DoFullWebServerFlow()(err error) {
 	return
 }
 
-func (oa *OAuthSecurity) DoFullUNFlow()(err error) {
+func (oa *OAuthSecurity) DoFullUNFlow() (err error) {
 	err = oa.GetAccessToken(GrantType_Password)
 	return
 }
 
-func (oa *OAuthSecurity) BuildAuthorizeURL(OAuthFlow int)(AuthorizeURL string) {
+func (oa *OAuthSecurity) BuildAuthorizeURL(OAuthFlow int) (AuthorizeURL string) {
 	if OAuthFlow_WebServer == OAuthFlow {
 		var AuthURL string = "%s/authorize?response_type=code&immediate=false&client_id=%s&redirect_uri=%s&scope=%s"
-	    AuthorizeURL = fmt.Sprintf(AuthURL, oa.OAuthBaseURL, url.QueryEscape(oa.ConsumerKey), url.QueryEscape(oa.ConsumerCallback), url.QueryEscape(oa.Scope))
+		AuthorizeURL = fmt.Sprintf(AuthURL, oa.OAuthBaseURL, url.QueryEscape(oa.ConsumerKey), url.QueryEscape(oa.ConsumerCallback), url.QueryEscape(oa.Scope))
 	}
 	if OAuthFlow_UserAgent == OAuthFlow {
 		var AuthURL string = "%s/authorize?response_type=token&client_id=%s&redirect_uri=%s&scope=%s"
@@ -100,142 +100,142 @@ func (oa *OAuthSecurity) BuildAuthorizeURL(OAuthFlow int)(AuthorizeURL string) {
 	return
 }
 
-func (oa *OAuthSecurity) GetAccessToken(GrantType int) (err error) {  
-    values := make(map[string]string)
-    myUrl := fmt.Sprintf("%s/token", oa.OAuthBaseURL)
+func (oa *OAuthSecurity) GetAccessToken(GrantType int) (err error) {
+	values := make(map[string]string)
+	myUrl := fmt.Sprintf("%s/token", oa.OAuthBaseURL)
 
-    if GrantType == GrantType_RefreshToken {
-        values["refresh_token"] = oa.RefreshToken
-        values["grant_type"] = "refresh_token"
-    }
-    if GrantType == GrantType_AuthorizationToken {
-        values["code"] = oa.AuthCode   
-        values["redirect_uri"] = oa.ConsumerCallback
-        values["grant_type"] = "authorization_code"
-    }
-    if GrantType == GrantType_Password {
-    	values["username"] = oa.UserName
-    	values["password"] = oa.Password
-    	values["grant_type"] = "password"
-    }
-    values["client_id"] = oa.ConsumerKey
-    values["client_secret"] = oa.ConsumerSecret
-    values["format"] = "json"
-    var res []byte
-    var requestBody []byte = []byte(UrlEncode(values))
-    if res, err = oa.Post(myUrl, requestBody, "application/x-www-form-urlencoded"); err != nil {
-        return err
-    }   
-    var result ForceResponse
-    json.Unmarshal(res, &result) 
-    oa.AccessToken = "" 
-    for idx, val := range(result) {   
-    	switch idx {
-    	case "access_token":
-    		oa.AccessToken = val.(string) 
-    	case "instance_url":
-    		oa.InstanceUrl = val.(string)
-    	case "refresh_token":
-    		oa.RefreshToken = val.(string)
-    	case "id":
-    		oa.Id = val.(string)
-    	case "issued_at":
-    		oa.IssuedAt = val.(string)
-    	case "signature":
-    		oa.Signature = val.(string)
-    	}      
-    }
-    return nil
+	if GrantType == GrantType_RefreshToken {
+		values["refresh_token"] = oa.RefreshToken
+		values["grant_type"] = "refresh_token"
+	}
+	if GrantType == GrantType_AuthorizationToken {
+		values["code"] = oa.AuthCode
+		values["redirect_uri"] = oa.ConsumerCallback
+		values["grant_type"] = "authorization_code"
+	}
+	if GrantType == GrantType_Password {
+		values["username"] = oa.UserName
+		values["password"] = oa.Password
+		values["grant_type"] = "password"
+	}
+	values["client_id"] = oa.ConsumerKey
+	values["client_secret"] = oa.ConsumerSecret
+	values["format"] = "json"
+	var res []byte
+	var requestBody []byte = []byte(UrlEncode(values))
+	if res, err = oa.Post(myUrl, requestBody, "application/x-www-form-urlencoded"); err != nil {
+		return err
+	}
+	var result ForceResponse
+	json.Unmarshal(res, &result)
+	oa.AccessToken = ""
+	for idx, val := range result {
+		switch idx {
+		case "access_token":
+			oa.AccessToken = val.(string)
+		case "instance_url":
+			oa.InstanceUrl = val.(string)
+		case "refresh_token":
+			oa.RefreshToken = val.(string)
+		case "id":
+			oa.Id = val.(string)
+		case "issued_at":
+			oa.IssuedAt = val.(string)
+		case "signature":
+			oa.Signature = val.(string)
+		}
+	}
+	return nil
 }
 
 func (oa *OAuthSecurity) Post(theUrl string, requestBody []byte, contentType string) (body []byte, err error) {
-    return oa.httpGo("POST", theUrl, requestBody, contentType)
+	return oa.httpGo("POST", theUrl, requestBody, contentType)
 }
 
 func (oa *OAuthSecurity) Get(theUrl string, contentType string) (body []byte, err error) {
-    return oa.httpGo("GET", theUrl, nil, contentType)
+	return oa.httpGo("GET", theUrl, nil, contentType)
 }
 
 func (oa *OAuthSecurity) Patch(theUrl string, requestBody []byte, contentType string) (body []byte, err error) {
-    return oa.httpGo("PATCH", theUrl, requestBody, contentType)
+	return oa.httpGo("PATCH", theUrl, requestBody, contentType)
 }
 
 func (oa *OAuthSecurity) Delete(theUrl string, contentType string) (body []byte, err error) {
-    return oa.httpGo("DELETE", theUrl, nil, contentType)
+	return oa.httpGo("DELETE", theUrl, nil, contentType)
 }
 
 func (oa *OAuthSecurity) httpGo(method string, theUrl string, requestBody []byte, contentType string) (body []byte, err error) {
-    var req *http.Request
-    if requestBody == nil {
-        req, err = httpRequest(method, theUrl, nil)
-    } else {
-        req, err = httpRequest(method, theUrl, bytes.NewReader(requestBody))
-    }
-    if err != nil {
-        return
-    }
-    if oa.AccessToken != "" {
-        req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", oa.AccessToken))
-    }
-    if contentType != "" {
-    	req.Header.Add("Content-Type", contentType)
-    } else {
-    	req.Header.Add("Content-Type", "application/json")
-    }
-    res, err := httpClient().Do(req)
-    if err != nil {
-        return
-    }
-    defer res.Body.Close()
-    if res.StatusCode == 401 {
-    	if oa.AutoRefreshToken && oa.RefreshToken != "" {
-        	oa.AutoRefreshToken = false
-        	oa.AccessToken = ""
-        	err = oa.GetAccessToken(GrantType_RefreshToken)
-        	if err != nil && oa.AccessToken != "" {
-        		body, err = oa.httpGo(method, theUrl, requestBody, contentType)
-        	}
-			oa.AutoRefreshToken = true        	
+	var req *http.Request
+	if requestBody == nil {
+		req, err = httpRequest(method, theUrl, nil)
+	} else {
+		req, err = httpRequest(method, theUrl, bytes.NewReader(requestBody))
+	}
+	if err != nil {
+		return
+	}
+	if oa.AccessToken != "" {
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", oa.AccessToken))
+	}
+	if contentType != "" {
+		req.Header.Add("Content-Type", contentType)
+	} else {
+		req.Header.Add("Content-Type", "application/json")
+	}
+	res, err := httpClient().Do(req)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	if res.StatusCode == 401 {
+		if oa.AutoRefreshToken && oa.RefreshToken != "" {
+			oa.AutoRefreshToken = false
+			oa.AccessToken = ""
+			err = oa.GetAccessToken(GrantType_RefreshToken)
+			if err != nil && oa.AccessToken != "" {
+				body, err = oa.httpGo(method, theUrl, requestBody, contentType)
+			}
+			oa.AutoRefreshToken = true
 			return
-        }
-        err = errors.New("authorization expired - could not refresh token")
-        return
-    }
-    body, err = ioutil.ReadAll(res.Body)
-    if res.StatusCode/100 != 2 {
-     	err = errors.New(fmt.Sprintf("Status Code was not 200 (%d) - %s", res.StatusCode, CharsToString(body)))
-        return
-    }
-    return
+		}
+		err = errors.New("authorization expired - could not refresh token")
+		return
+	}
+	body, err = ioutil.ReadAll(res.Body)
+	if res.StatusCode/100 != 2 {
+		err = errors.New(fmt.Sprintf("Status Code was not 200 (%d) - %s", res.StatusCode, CharsToString(body)))
+		return
+	}
+	return
 }
 
 func UrlEncode(attrs map[string]string) (retUrl string) {
-    retUrl = ""
-    for key, val := range(attrs) {
-        if len(retUrl) > 0 {
-            retUrl += "&"
-        }
-        retUrl += url.QueryEscape(key) + "=" + url.QueryEscape(val)
-    }    
-    return
+	retUrl = ""
+	for key, val := range attrs {
+		if len(retUrl) > 0 {
+			retUrl += "&"
+		}
+		retUrl += url.QueryEscape(key) + "=" + url.QueryEscape(val)
+	}
+	return
 }
 
 func httpRequest(method, url string, body io.Reader) (request *http.Request, err error) {
-    request, err = http.NewRequest(method, url, body)
-    if err != nil {
-        return
-    }
-    request.Header.Add("User-Agent", "github.com/haagen/forceGOAuth")
-    return
+	request, err = http.NewRequest(method, url, body)
+	if err != nil {
+		return
+	}
+	request.Header.Add("User-Agent", "github.com/haagen/forceGOAuth")
+	return
 }
 
 func httpClient() (client *http.Client) {
 	if HTTPClient == nil {
-    	client = &http.Client{}
-    } else {
-    	client = HTTPClient
-    }
-    return
+		client = &http.Client{}
+	} else {
+		client = HTTPClient
+	}
+	return
 }
 
 // Stolen from force.com CLI -- github.com/heroku/force
@@ -273,17 +273,17 @@ var outputTemplate = template.Must(template.New("error").Parse(`
 `))
 
 func PrintSuccess(w http.ResponseWriter, s string) {
-    outputTemplate.Execute(w, s)
+	outputTemplate.Execute(w, s)
 }
 
-func CharsToString(ca []byte) string { 
-        s := make([]byte, len(ca)) 
-        var lens int 
-        for ; lens < len(ca); lens++ { 
-                if ca[lens] == 0 { 
-                        break 
-                } 
-                s[lens] = uint8(ca[lens]) 
-        } 
-        return string(s[0:lens]) 
-} 
+func CharsToString(ca []byte) string {
+	s := make([]byte, len(ca))
+	var lens int
+	for ; lens < len(ca); lens++ {
+		if ca[lens] == 0 {
+			break
+		}
+		s[lens] = uint8(ca[lens])
+	}
+	return string(s[0:lens])
+}
--- forceGOAuth_test.go
+++ forceGOAuth_test.go
@@ -1,19 +1,222 @@
 package forceGOAuth
 
 import (
+	"fmt"
 	"github.com/bmizerany/assert"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
+
+var (
+	mux    *http.ServeMux
+	server *httptest.Server
+	client *OAuthSecurity
+)
+
+func setup() {
+	mux = http.NewServeMux()
+	server = httptest.NewServer(mux)
+	client = NewOAuthSecurity()
+	client.OAuthBaseURL = server.URL
+}
+
+func teardown() {
+	server.Close()
+}
+
+func testMethod(t *testing.T, r *http.Request, want string) {
+	if want != r.Method {
+		t.Errorf("Request method = %v, want %v", r.Method, want)
+	}
+}
+
+type values map[string]string
+
+func testFormValues(t *testing.T, r *http.Request, values values) {
+	for key, want := range values {
+		if v := r.FormValue(key); v != want {
+			t.Errorf("Request parameter %v = %v, want %v", key, v, want)
+		}
+	}
+}
 
 // test that all avialable commands come with at least a name and short usage information
 func Test_BuildAuthorizeURL(t *testing.T) {
 	oa := NewOAuthSecurity()
 	oa.OAuthBaseURL = "https://test.salesforce.com/services/oauth2"
 	oa.ConsumerKey = "XXXX"
-	oa.ConsumerSecret = "YYYY"	
+	oa.ConsumerSecret = "YYYY"
 	oa.ConsumerCallback = "ZZZZ"
 	oa.Scope = "SSSS"
 
 	assert.Equal(t, oa.BuildAuthorizeURL(OAuthFlow_WebServer), oa.OAuthBaseURL+"/authorize?response_type=code&immediate=false&client_id=XXXX&redirect_uri=ZZZZ&scope=SSSS")
 	assert.Equal(t, oa.BuildAuthorizeURL(OAuthFlow_UserAgent), oa.OAuthBaseURL+"/authorize?response_type=token&client_id=XXXX&redirect_uri=ZZZZ&scope=SSSS")
 }
+
+func Test_GetAccessToken_AccessToken(t *testing.T) {
+	setup()
+	defer teardown()
+
+	client.AuthCode = "TheAuthCode"
+	client.ConsumerCallback = "TheCallBack"
+	client.ConsumerKey = "TheConsumerKey"
+	client.ConsumerSecret = "TheConsumerSecret"
+	mux.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		testFormValues(t, r, values{
+			"code":          "TheAuthCode",
+			"redirect_uri":  "TheCallBack",
+			"grant_type":    "authorization_code",
+			"client_id":     "TheConsumerKey",
+			"client_secret": "TheConsumerSecret",
+			"format":        "json",
+		})
+		fmt.Fprint(w, `
+{
+   "id":"TheId",
+   "issued_at":"TheIssuedAt",
+   "scope":"full refresh_token",
+   "instance_url":"TheInstanceURL",
+   "token_type":"Bearer",
+   "refresh_token":"TheRefreshToken",
+   "id_token":"TheIdToken",
+   "signature":"TheSignature",
+   "access_token":"TheAccessToken"
+}`)
+	})
+
+	err := client.GetAccessToken(GrantType_AuthorizationToken)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, client.AccessToken, "TheAccessToken")
+	assert.Equal(t, client.InstanceUrl, "TheInstanceURL")
+	assert.Equal(t, client.RefreshToken, "TheRefreshToken")
+	assert.Equal(t, client.Id, "TheId")
+	assert.Equal(t, client.IssuedAt, "TheIssuedAt")
+	assert.Equal(t, client.Signature, "TheSignature")
+}
+
+func Test_GetAccessToken_DoFullUNFlow(t *testing.T) {
+	setup()
+	defer teardown()
+
+	client.AuthCode = "TheAuthCode"
+	client.ConsumerCallback = "TheCallBack"
+	client.ConsumerKey = "TheConsumerKey"
+	client.ConsumerSecret = "TheConsumerSecret"
+	client.UserName = "TheUsername"
+	client.Password = "ThePassword"
+	mux.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		testFormValues(t, r, values{
+			"grant_type":    "password",
+			"client_id":     "TheConsumerKey",
+			"client_secret": "TheConsumerSecret",
+			"format":        "json",
+			"username":      "TheUsername",
+			"password":      "ThePassword",
+		})
+		fmt.Fprint(w, `
+{
+   "id":"TheId",
+   "issued_at":"TheIssuedAt",
+   "scope":"full refresh_token",
+   "instance_url":"TheInstanceURL",
+   "token_type":"Bearer",
+   "refresh_token":"TheRefreshToken",
+   "id_token":"TheIdToken",
+   "signature":"TheSignature",
+   "access_token":"TheAccessToken"
+}`)
+	})
+
+	err := client.DoFullUNFlow()
+	assert.Equal(t, err, nil)
+	assert.Equal(t, client.AccessToken, "TheAccessToken")
+	assert.Equal(t, client.InstanceUrl, "TheInstanceURL")
+	assert.Equal(t, client.RefreshToken, "TheRefreshToken")
+	assert.Equal(t, client.Id, "TheId")
+	assert.Equal(t, client.IssuedAt, "TheIssuedAt")
+	assert.Equal(t, client.Signature, "TheSignature")
+}
+
+func callLocalServer() {
+	resp, err := http.Get(client.ConsumerCallback + "/?code=TheCode")
+	if err != nil {
+		defer resp.Body.Close()
+	}
+}
+
+func Test_LocalServer(t *testing.T) {
+
+	client.ConsumerCallback = "http://localhost:" + PORT
+	ch := make(chan OAuthSecurity)
+	_, err := startLocalHttpServer(ch)
+	assert.Equal(t, err, nil)
+
+	go callLocalServer()
+	var out OAuthSecurity = <-ch
+	assert.Equal(t, out.AuthCode, "TheCode")
+}
+
+func Test_LoadSave_Settings(t *testing.T) {
+
+	client.ConsumerKey = "ConsumerKey"
+	client.ConsumerSecret = "ConsumerSecret"
+	client.ConsumerCallback = "ConsumerCallback"
+	client.OAuthBaseURL = "OAuthBaseURL"
+	client.AuthCode = "AuthCode"
+	client.AccessToken = "AccessToken"
+	client.RefreshToken = "RefreshToken"
+	client.InstanceUrl = "InstanceUrl"
+	client.Id = "Id"
+	client.IssuedAt = "IssuedAt"
+	client.Signature = "Signature"
+	client.Scope = "Scope"
+	client.AutoRefreshToken = true
+	client.UserName = "UserName"
+	client.Password = "Password"
+
+	newClient := NewOAuthSecurity()
+	settings, err := client.ExportSettingsJSON()
+	assert.Equal(t, err, nil)
+	err = newClient.ImportSettingsJSON(settings)
+	assert.Equal(t, err, nil)
+
+	assert.Equal(t, newClient.ConsumerKey, "ConsumerKey")
+	assert.Equal(t, newClient.ConsumerSecret, "ConsumerSecret")
+	assert.Equal(t, newClient.ConsumerCallback, "ConsumerCallback")
+	assert.Equal(t, newClient.OAuthBaseURL, "OAuthBaseURL")
+	assert.Equal(t, newClient.AuthCode, "AuthCode")
+	assert.Equal(t, newClient.AccessToken, "AccessToken")
+	assert.Equal(t, newClient.RefreshToken, "RefreshToken")
+	assert.Equal(t, newClient.InstanceUrl, "InstanceUrl")
+	assert.Equal(t, newClient.Id, "Id")
+	assert.Equal(t, newClient.IssuedAt, "IssuedAt")
+	assert.Equal(t, newClient.Signature, "Signature")
+	assert.Equal(t, newClient.Scope, "Scope")
+	assert.Equal(t, newClient.AutoRefreshToken, true)
+	assert.Equal(t, newClient.UserName, "UserName")
+	assert.Equal(t, newClient.Password, "Password")
+
+}
+
+func Test_CharsToString(t *testing.T) {
+
+	bytes := []byte{97, 98, 99, 100, 101}
+	assert.Equal(t, CharsToString(bytes), "abcde")
+
+}
+
+/*
+Response for GetAccessToken for RefreshToken
+{
+   "id":"TheId",
+   "issued_at":"TheIssuedAt",
+   "scope":"full refresh_token",
+   "instance_url":"TheInstanceURL",
+   "token_type":"Bearer",
+   "signature":"TheSignature",
+   "access_token":"TheAccessToken"
+}
+*/
